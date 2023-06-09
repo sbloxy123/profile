991a6e51dc2567a6efecc03574f7a57edf2aa433
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2023-06-09 11:01:31 +0100

    fix redirection to new site

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  alert(
-    "\nOops! It seems you've come to my old website...\n I'll just send you to my new one 😉"
-  )
-);
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   alert(
+//     "\nOops! It seems you've come to my old website...\n I'll just send you to my new one 😉"
+//   )
+// );
